fdc5c31da9e925b623a4fff619e681eeb5711871
rahulrajdahal <rahulrajdahal@gmail.com>
2024-04-24 18:46:19 +0545

    test: landing

--- cypress/e2e/LandingPage.cy.ts
+++ cypress/e2e/LandingPage.cy.ts
@@ -1,40 +1,51 @@
 describe('Landing Page E2E Test', () => {
-  it('should visit the domain url and render landing page texts.', () => {
-    cy.visit('/');
+  beforeEach(() => {
+    fetchForecast();
+  });
 
+  it('should visit the domain url and render landing page texts.', () => {
     cy.get('.py-5').should('contain.text', 'WeatherApp');
     cy.get('.md\\:flex-row > .justify-between').should('be.visible');
     cy.get('.mt-0').should('contain.text', 'The current weather in');
   });
 
-  it('should visit the domain url and fetch the current weather.', () => {
-    let latitude = 27.7293;
-    let longitude = 85.3343;
+  it('should visit the domain url and fetch the current weather.', function () {
+    expect(this.currentForecast).to.haveOwnProperty('condition');
+    expect(this.currentForecast)
+      .to.haveOwnProperty('condition')
+      .to.haveOwnProperty('text');
+    expect(this.currentForecast)
+      .to.haveOwnProperty('condition')
+      .to.haveOwnProperty('icon');
 
-    cy.visit('/');
-
-    cy.intercept(
-      `https://api.weatherapi.com/v1/forecast.json?q=${latitude},${longitude}&days=2&key=${Cypress.env('NEXT_PUBLIC_WEATHER_APIKEY')}`,
-      (req) =>
-        req.continue((res) => {
-          const body = res.body;
-          expect(body).to.haveOwnProperty('current');
-          expect(body).to.haveOwnProperty('forecast');
-          expect(body).to.haveOwnProperty('location');
-
-          const current = body.current;
-
-          expect(current).to.haveOwnProperty('condition');
-          expect(current).to.haveOwnProperty('humidity');
-          expect(current).to.haveOwnProperty('temp_c');
-          expect(current).to.haveOwnProperty('time');
-          expect(current).to.haveOwnProperty('tz_id');
-          expect(current).to.haveOwnProperty('wind_kph');
-
-          const forecast = body.forecast;
-
-          expect(forecast).to.haveOwnProperty('forecastday');
-        })
-    );
+    expect(this.currentForecast).to.haveOwnProperty('humidity');
+    expect(this.currentForecast).to.haveOwnProperty('temp_c');
+    expect(this.currentForecast).to.haveOwnProperty('last_updated');
+    expect(this.currentForecast).to.haveOwnProperty('wind_kph');
+  });
+  it('should visit the domain url and fetch the current weather location.', function () {
+    expect(this.currentLocation).to.haveOwnProperty('tz_id');
+    expect(this.currentLocation).to.haveOwnProperty('name');
+    expect(this.currentLocation).to.haveOwnProperty('country');
   });
 });
+
+const fetchForecast = () => {
+  let latitude = 27.7293;
+  let longitude = 85.3343;
+
+  cy.visit('/');
+
+  cy.intercept(
+    `https://api.weatherapi.com/v1/forecast.json?q=${latitude},${longitude}&days=2&key=${Cypress.env('NEXT_PUBLIC_WEATHER_APIKEY')}`
+  ).as('getForecast');
+
+  cy.wait('@getForecast')
+    .its('response.body')
+    .its('current')
+    .as('currentForecast');
+  cy.wait('@getForecast')
+    .its('response.body')
+    .its('location')
+    .as('currentLocation');
+};
